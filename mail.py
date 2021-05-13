c9ae6b353afd434ada5ce4724b5ee4fc5b257f31
OGURCHINSKIY <41481736+OGURCHINSKIY@users.noreply.github.com>
2021-05-13 04:49:54 +0300

    Update mail.py
    2.0

--- mail.py
+++ mail.py
@@ -1,41 +1,54 @@
 import asyncio
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
+from aiogram import types
+from aiogram.dispatcher.handler import CancelHandler, _check_spec, SkipHandler
+from aiogram.dispatcher.middlewares import BaseMiddleware
+from aiogram.dispatcher.filters import check_filters, FilterNotPassed
 
 
-bot = Bot(token='')
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
+class CollectAlbum(BaseMiddleware):
+    def __init__(self, delay=1):
+        super().__init__()
+        self.albums = {}
+        self.timerHandles = {}
+        self.delay = delay
 
+    async def notify(self, message: types.Message):
+        data = {}
+        for handler_obj in self.manager.dispatcher.message_handlers.handlers:
+            try:
+                data.update(await check_filters(handler_obj.filters, (message,)))
+            except FilterNotPassed:
+                continue
+            else:
+                try:
+                    data[handler_obj.spec.args[0]] = self.albums.get(message.media_group_id)  # 🤢
+                    partial_data = _check_spec(handler_obj.spec, data)
+                    await handler_obj.handler(**partial_data)
+                except SkipHandler:
+                    continue
+                except CancelHandler:
+                    break
+                finally:
+                    break
 
-async def on_group_photo(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        await message.answer(f"Media group: {data[message.media_group_id]}")
+    async def on_pre_process_message(self, message: types.Message, data: dict):
+        if not message.media_group_id:
+            return
+        self.albums.setdefault(message.media_group_id, []).append(message)
+        await self.adaptive_delay(message)
+        raise CancelHandler()
 
-
-@dp.message_handler(lambda message: message.media_group_id is not None, content_types="photo")
-async def album_handle(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        if message.media_group_id not in data:
-            asyncio.get_event_loop().call_later(1, asyncio.create_task, on_group_photo(message, state))
-        # data._data.setdefault(message.media_group_id, []).append(message.photo[-1].file_id) # for version <2.11.2
-        data.setdefault(message.media_group_id, []).append(message.photo[-1].file_id)
-
-
-async def on_group_document(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        for file in data[message.media_group_id]:
-            await file.download()
-
-
-@dp.message_handler(lambda message: message.media_group_id is not None, content_types="document")
-async def album_handle(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        if message.media_group_id not in data:
-            asyncio.get_event_loop().call_later(1, asyncio.create_task, on_group_document(message, state))
-        # data._data.setdefault(message.media_group_id, []).append(message.document) # for version <2.11.2
-        data.setdefault(message.media_group_id, []).append(message.document)
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    async def adaptive_delay(self, message: types.Message):
+        if len(self.albums.get(message.media_group_id, [])) == 1:
+            self.timerHandles[message.media_group_id] = asyncio.get_event_loop().call_later(
+                    self.delay,
+                    asyncio.create_task,
+                    self.notify(message)
+                )
+        else:
+            self.timerHandles[message.media_group_id].cancel()
+            self.timerHandles[message.media_group_id] = asyncio.get_event_loop().call_later(
+                    self.delay,
+                    asyncio.create_task,
+                    self.notify(message)
+                )
